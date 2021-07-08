486db821190f63836666f253f63430fefc958c06
aditya011rathore <87127877+aditya011rathore@users.noreply.github.com>
2021-07-08 18:28:00 +0530

    Delete admin.py

--- blog/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class PostAdmin(admin.ModelAdmin):
-	list_display = ['created_at', 'user', 'title','updated_at']
-
-admin.site.register(Post,PostAdmin)
